99fadbe25f8df679ed8b2416c27b63b023e307d1
Vitalii Hrytsiienko <v.hrytsiienko@gmail.com>
2022-04-14 15:13:23 +0300

    escape values

--- src/formatter/index.ts
+++ src/formatter/index.ts
@@ -2,6 +2,22 @@ import * as sqlString from "sqlstring";
 import { checkArgumentValid } from "../common/util";
 import { INVALID_REPLACEMENT, INVALID_REPLACEMENTS } from "../common/errors";
 
+const removeComments = (query: string) => {
+  query = query.replace(
+    /("(""|[^"])*")|('(''|[^'])*')|(--[^\n\r]*)|(\/\*[\w\W]*?(?=\*\/)\*\/)/gm,
+    match => {
+      if (
+        (match[0] === '"' && match[match.length - 1] === '"') ||
+        (match[0] === "'" && match[match.length - 1] === "'")
+      )
+        return match;
+
+      return "";
+    }
+  );
+  return query;
+};
+
 export class QueryFormatter {
   private checkReplacements(replacements: unknown[]) {
     checkArgumentValid(Array.isArray(replacements), INVALID_REPLACEMENTS);
@@ -14,15 +30,94 @@ export class QueryFormatter {
     }
   }
 
-  private fillReplacements(query: string, replacements: unknown[]): string {
-    return sqlString.format(query, replacements);
+  private format(sql: string, params: unknown[]) {
+    const regex = /(''|""|``|\\\\|\\'|\\"|'|"|`|\?)/g;
+
+    const STATE = {
+      WHITESPACE: 0,
+      SINGLE_QUOTE: 1,
+      DOUBLE_QUOTE: 2,
+      BACKTICK: 3
+    };
+
+    const stateSwitches: Record<string, number> = {
+      "'": STATE.SINGLE_QUOTE,
+      '"': STATE.DOUBLE_QUOTE,
+      "`": STATE.BACKTICK
+    };
+
+    let state = STATE.WHITESPACE;
+
+    sql = sql.replace(regex, str => {
+      if (str in stateSwitches) {
+        if (state === STATE.WHITESPACE) {
+          state = stateSwitches[str];
+        } else if (state === stateSwitches[str]) {
+          state = STATE.WHITESPACE;
+        }
+      }
+
+      if (str !== "?") {
+        return str;
+      }
+
+      if (state !== STATE.WHITESPACE) return str;
+
+      if (params.length == 0) {
+        throw new Error("Too few parameters given");
+      }
+
+      return this.escape(params.shift());
+    });
+
+    if (params.length) {
+      throw new Error("Too many parameters given");
+    }
+
+    return sql;
+  }
+
+  escape(param: unknown) {
+    if (param === undefined || param === null) {
+      return "NULL";
+    }
+
+    switch (typeof param) {
+      case "boolean": {
+        return param ? "true" : "false";
+      }
+      case "number": {
+        return param.toString();
+      }
+      case "object": {
+        return this.escapeObject(param);
+      }
+
+      default: {
+        return this.escapeString(param);
+      }
+    }
+    return "" + param;
+  }
+
+  escapeString(param: unknown) {
+    return "" + param;
+  }
+
+  escapeObject(param: unknown) {
+    if (Array.isArray(param)) {
+      return "array";
+    }
+
+    return "object";
   }
 
   formatQuery(query: string, replacements: unknown[]): string {
     if (replacements) {
       this.checkReplacements(replacements);
-      const withReplacements = this.fillReplacements(query, replacements);
-      return withReplacements;
+      query = removeComments(query);
+      query = this.format(query, replacements);
+      return query;
     }
     return query;
   }
--- test/unit/statement.test.ts
+++ test/unit/statement.test.ts
@@ -15,4 +15,20 @@ describe("format query", () => {
       `"select 1 from table where bar = 2"`
     );
   });
+  it("format 3", () => {
+    const queryFormatter = new QueryFormatter();
+    const query = "select * from table where bar = ? and foo = `some'?`";
+    const formattedQuery = queryFormatter.formatQuery(query, [1]);
+    expect(formattedQuery).toMatchInlineSnapshot(
+      `"select * from table where bar = 1 and foo = \`some'?\`"`
+    );
+  });
+  it("escape boolean", () => {
+    const queryFormatter = new QueryFormatter();
+    const query = "select * from table where bar = ?;";
+    const formattedQuery = queryFormatter.formatQuery(query, [true]);
+    expect(formattedQuery).toMatchInlineSnapshot(
+      `"select * from table where bar = true;"`
+    );
+  });
 });
