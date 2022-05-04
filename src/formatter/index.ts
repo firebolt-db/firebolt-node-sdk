ffebe18793a934777281b01d6442362d58353c11
Vitalii Hrytsiienko <v.hrytsiienko@gmail.com>
2022-05-04 17:50:14 +0300

    cover other types

--- src/formatter/index.ts
+++ src/formatter/index.ts
@@ -1,5 +1,20 @@
+import BigNumber from "bignumber.js";
 import { checkArgumentValid } from "../common/util";
 import { INVALID_REPLACEMENT, INVALID_REPLACEMENTS } from "../common/errors";
+
+const CHARS_GLOBAL_REGEXP = /[\0\b\t\n\r\x1a"'\\]/g; // eslint-disable-line no-control-regex
+
+const CHARS_ESCAPE_MAP: Record<string, string> = {
+  "\0": "\\0",
+  "\b": "\\b",
+  "\t": "\\t",
+  "\n": "\\n",
+  "\r": "\\r",
+  "\x1a": "\\Z",
+  '"': '\\"',
+  "'": "\\'",
+  "\\": "\\\\"
+};
 
 const removeComments = (query: string) => {
   query = query.replace(
@@ -17,6 +32,15 @@ const removeComments = (query: string) => {
   return query;
 };
 
+const zeroPad = (param: number, length: number) => {
+  let paded = param.toString();
+  while (paded.length < length) {
+    paded = "0" + paded;
+  }
+
+  return paded;
+};
+
 export class QueryFormatter {
   private checkReplacements(replacements: unknown[]) {
     checkArgumentValid(Array.isArray(replacements), INVALID_REPLACEMENTS);
@@ -29,7 +53,7 @@ export class QueryFormatter {
     }
   }
 
-  private format(sql: string, params: unknown[]) {
+  private format(query: string, params: unknown[]) {
     const regex = /(''|""|``|\\\\|\\'|\\"|'|"|`|\?)/g;
 
     const STATE = {
@@ -47,7 +71,7 @@ export class QueryFormatter {
 
     let state = STATE.WHITESPACE;
 
-    sql = sql.replace(regex, str => {
+    query = query.replace(regex, str => {
       if (str in stateSwitches) {
         if (state === STATE.WHITESPACE) {
           state = stateSwitches[str];
@@ -63,20 +87,20 @@ export class QueryFormatter {
       if (state !== STATE.WHITESPACE) return str;
 
       if (params.length == 0) {
-        throw new Error("Too few parameters given");
+        throw new Error("Too few replacements given");
       }
 
       return this.escape(params.shift());
     });
 
     if (params.length) {
-      throw new Error("Too many parameters given");
+      throw new Error("Too many replacements given");
     }
 
-    return sql;
+    return query;
   }
 
-  escape(param: unknown) {
+  private escape(param: unknown) {
     if (param === undefined || param === null) {
       return "NULL";
     }
@@ -92,31 +116,113 @@ export class QueryFormatter {
         return this.escapeObject(param);
       }
 
-      default: {
+      case "string": {
         return this.escapeString(param);
       }
+      default: {
+        return "" + param;
+      }
     }
-    return "" + param;
   }
 
-  escapeString(param: unknown) {
-    return "" + param;
-  }
+  private escapeString(param: string) {
+    let chunkIndex = (CHARS_GLOBAL_REGEXP.lastIndex = 0);
+    let escapedValue = "";
+    let match;
 
-  escapeObject(param: unknown) {
-    if (Array.isArray(param)) {
-      return "array";
+    while ((match = CHARS_GLOBAL_REGEXP.exec(param))) {
+      const key = match[0];
+      escapedValue +=
+        param.slice(chunkIndex, match.index) + CHARS_ESCAPE_MAP[key];
+      chunkIndex = CHARS_GLOBAL_REGEXP.lastIndex;
     }
 
-    return "object";
+    if (chunkIndex === 0) {
+      // Nothing was escaped
+      return "'" + param + "'";
+    }
+
+    if (chunkIndex < param.length) {
+      return "'" + escapedValue + param.slice(chunkIndex) + "'";
+    }
+
+    return "'" + escapedValue + "'";
+  }
+
+  private escapeBuffer(param: Buffer) {
+    return "X" + this.escapeString(param.toString("hex"));
+  }
+
+  private escapeArray(param: unknown[]) {
+    let sql = "[";
+
+    for (let i = 0; i < param.length; i++) {
+      const value = param[i];
+      const prefix = i === 0 ? "" : ", ";
+
+      if (Array.isArray(value)) {
+        sql += prefix + this.escapeArray(value);
+      } else {
+        sql += prefix + this.escape(value);
+      }
+    }
+
+    sql += "]";
+
+    return sql;
+  }
+
+  private escapeDate(param: Date) {
+    const dt = new Date(param);
+
+    if (isNaN(dt.getTime())) {
+      return "NULL";
+    }
+
+    const year = dt.getFullYear();
+    const month = dt.getMonth() + 1;
+    const day = dt.getDate();
+    const hour = dt.getHours();
+    const minute = dt.getMinutes();
+    const second = dt.getSeconds();
+
+    // YYYY-MM-DD HH:mm:ss.mmm
+    const str =
+      zeroPad(year, 4) +
+      "-" +
+      zeroPad(month, 2) +
+      "-" +
+      zeroPad(day, 2) +
+      " " +
+      zeroPad(hour, 2) +
+      ":" +
+      zeroPad(minute, 2) +
+      ":" +
+      zeroPad(second, 2);
+    //+ "." + zeroPad(millisecond, 3);
+
+    return this.escapeString(str);
+  }
+
+  private escapeObject(param: unknown) {
+    if (BigNumber.isBigNumber(param)) {
+      return param.toString();
+    } else if (Object.prototype.toString.call(param) === "[object Date]") {
+      return this.escapeDate(param as Date);
+    } else if (Array.isArray(param)) {
+      return this.escapeArray(param);
+    } else if (Buffer.isBuffer(param)) {
+      return this.escapeBuffer(param);
+    } else {
+      return this.escapeString("" + param);
+    }
   }
 
   formatQuery(query: string, replacements: unknown[]): string {
+    query = removeComments(query);
     if (replacements) {
       this.checkReplacements(replacements);
-      query = removeComments(query);
       query = this.format(query, replacements);
-      return query;
     }
     return query;
   }
--- test/unit/statement.test.ts
+++ test/unit/statement.test.ts
@@ -1,3 +1,4 @@
+import BigNumber from "bignumber.js";
 import { QueryFormatter } from "../../src/formatter";
 
 describe("format query", () => {
@@ -37,5 +38,78 @@ describe("format query", () => {
     const query = "select ? from table";
     const formattedQuery = queryFormatter.formatQuery(query, ["foo?"]);
     expect(formattedQuery).toMatchInlineSnapshot(`"select 'foo?' from table"`);
+  });
+  it("format \n", () => {
+    const queryFormatter = new QueryFormatter();
+    const query = "select ? from table";
+    const formattedQuery = queryFormatter.formatQuery(query, ["foo\nbar"]);
+    expect(formattedQuery).toMatchInlineSnapshot(
+      `"select 'foo\\\\nbar' from table"`
+    );
+  });
+  it("format '", () => {
+    const queryFormatter = new QueryFormatter();
+    const query = "select ? from table";
+    const formattedQuery = queryFormatter.formatQuery(query, ["foo'bar"]);
+    expect(formattedQuery).toMatchInlineSnapshot(
+      `"select 'foo\\\\'bar' from table"`
+    );
+  });
+
+  it("format '", () => {
+    const queryFormatter = new QueryFormatter();
+    const query = "select ? from table";
+    const formattedQuery = queryFormatter.formatQuery(query, ["foo`bar"]);
+    expect(formattedQuery).toMatchInlineSnapshot(
+      `"select 'foo\`bar' from table"`
+    );
+  });
+  it("format array", () => {
+    const queryFormatter = new QueryFormatter();
+    const query = "select ? from table";
+    const formattedQuery = queryFormatter.formatQuery(query, [["foo", 'bar"']]);
+    expect(formattedQuery).toMatchInlineSnapshot(
+      `"select ['foo', 'bar\\\\\\"'] from table"`
+    );
+  });
+  it("format nested array", () => {
+    const queryFormatter = new QueryFormatter();
+    const query = "select ? from table";
+    const formattedQuery = queryFormatter.formatQuery(query, [
+      ["foo", ["foo", 'bar"']]
+    ]);
+    expect(formattedQuery).toMatchInlineSnapshot(
+      `"select ['foo', ['foo', 'bar\\\\\\"']] from table"`
+    );
+  });
+  it("escape bignumber", () => {
+    const queryFormatter = new QueryFormatter();
+    const query = "select ? from table";
+    const num = new BigNumber("688887797400064883");
+    const formattedQuery = queryFormatter.formatQuery(query, [num]);
+    expect(formattedQuery).toMatchInlineSnapshot(
+      `"select 688887797400064883 from table"`
+    );
+  });
+  it("format date", () => {
+    const queryFormatter = new QueryFormatter();
+    const query = "select ? from table";
+    const formattedQuery = queryFormatter.formatQuery(query, [
+      new Date("2022-05-04 17:37:19")
+    ]);
+    expect(formattedQuery).toMatchInlineSnapshot(
+      `"select '2022-05-04 17:37:19' from table"`
+    );
+  });
+
+  it("format date", () => {
+    const queryFormatter = new QueryFormatter();
+    const query = "select ? from table";
+    const formattedQuery = queryFormatter.formatQuery(query, [
+      new Date("1000-01-01 12:21:21")
+    ]);
+    expect(formattedQuery).toMatchInlineSnapshot(
+      `"select '1000-01-01 12:21:21' from table"`
+    );
   });
 });
