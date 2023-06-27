3834479e128478e746688898ed57a0fba509a498
ptiurin <petro.tiurin@firebolt.io>
2023-06-27 14:59:37 +0100

    change const names

--- src/common/errors.ts
+++ src/common/errors.ts
@@ -1,13 +1,13 @@
-export const MISSING_USERNAME = 404001;
-export const MISSING_PASSWORD = 404002;
+export const MISSING_CLIENT_ID = 404001;
+export const MISSING_CLIENT_SECRET = 404002;
 export const MISSING_DATABASE = 404003;
 export const MISSING_ENGINE_ENDPOINT = 404004;
 
 export const INVALID_PARAMETERS = 400001;
 
 const errorMessages: Record<number, string> = {
-  [MISSING_PASSWORD]: "Password is missing",
-  [MISSING_USERNAME]: "Username is missing",
+  [MISSING_CLIENT_ID]: "Password is missing",
+  [MISSING_CLIENT_SECRET]: "Username is missing",
   [MISSING_DATABASE]: "Database is missing",
   [MISSING_ENGINE_ENDPOINT]:
     "At least one should be provided: engineName or engineEndpoint",
