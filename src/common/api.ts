86c1ba22fe4449fdd43b6491a036a254e1757ef7
ptiurin <petro.tiurin@firebolt.io>
2023-07-19 17:52:06 +0100

    revert query change

--- src/common/api.ts
+++ src/common/api.ts
@@ -1,6 +1,6 @@
 export const SERVICE_ACCOUNT_LOGIN = "oauth/token";
 
-export const QUERY_URL = "query";
+export const QUERY_URL = "dynamic/query";
 export const ACCOUNT_SYSTEM_ENGINE = (accountName: string) =>
   `web/v3/account/${accountName}/engineUrl`;
 export const ACCOUNT_ID_BY_NAME = (accountName: string) =>
