dec1603db83d29ae011356be49c0a983ddc31439
Stepan Burlakov <stepansergeevitch@gmail.com>
2023-12-06 15:34:03 +0200

    remove account service

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,7 +1,6 @@
 import { LoggerInterface } from "../logger";
 import { DatabaseService } from "./database";
 import { EngineService } from "./engine";
-import { AccountService } from "./account";
 import { ResourceManagerContext } from "../types";
 import { Connection } from "../connection";
 
@@ -10,12 +9,10 @@ export class ResourceManager {
   accountName!: string;
   engine: EngineService;
   database: DatabaseService;
-  account: AccountService;
 
   constructor(context: { logger: LoggerInterface; connection: Connection }) {
     this.context = context;
     this.engine = new EngineService(this.context);
     this.database = new DatabaseService(this.context);
-    this.account = new AccountService(this.context);
   }
 }
