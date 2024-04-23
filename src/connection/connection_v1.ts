31fc93845e3a6a80b5bcc434c0b1c6a6a05d9baf
Stepan Burlakov <stepansergeevitch@gmail.com>
2024-04-23 12:42:48 +0300

    refactor account resolution

--- src/connection/connection_v1.ts
+++ src/connection/connection_v1.ts
@@ -3,9 +3,6 @@ import { Connection as BaseConnection } from "./base";
 import { ResourceManager } from "../service";
 
 export class ConnectionV1 extends BaseConnection {
-  // Cache for resolveAccountId
-  private accountIdCache: string | undefined;
-
   async resolveEngineEndpoint() {
     const resourceManager = new ResourceManager({
       connection: this,
@@ -28,29 +25,27 @@ export class ConnectionV1 extends BaseConnection {
     return this.engineEndpoint;
   }
 
-  async resolveAccountId() {
-    if (this.accountIdCache) {
-      return this.accountIdCache;
+  async resolveAccountInfo() {
+    if (this.accountInfo === undefined) {
+      const { httpClient, apiEndpoint } = this.context;
+      const { account } = this.options;
+      if (account) {
+        const queryParams = new URLSearchParams({ account_name: account });
+        const url = `${apiEndpoint}/${ACCOUNT_BY_NAME}?${queryParams}`;
+        const { account_id } = await httpClient
+          .request<{ account_id: string }>("GET", url)
+          .ready();
+        this.accountInfo = { id: account_id, infraVersion: 1 };
+      } else {
+        const url = `${apiEndpoint}/${ACCOUNT}`;
+        const {
+          account: { id }
+        } = await httpClient
+          .request<{ account: { id: string } }>("GET", url)
+          .ready();
+        this.accountInfo = { id, infraVersion: 1 };
+      }
     }
-    const { httpClient, apiEndpoint } = this.context;
-    const { account } = this.options;
-    if (account) {
-      const queryParams = new URLSearchParams({ account_name: account });
-      const url = `${apiEndpoint}/${ACCOUNT_BY_NAME}?${queryParams}`;
-      const { account_id } = await httpClient
-        .request<{ account_id: string }>("GET", url)
-        .ready();
-      this.accountIdCache = account_id;
-      return account_id;
-    } else {
-      const url = `${apiEndpoint}/${ACCOUNT}`;
-      const {
-        account: { id }
-      } = await httpClient
-        .request<{ account: { id: string } }>("GET", url)
-        .ready();
-      this.accountIdCache = id;
-      return id;
-    }
+    return this.accountInfo;
   }
 }
