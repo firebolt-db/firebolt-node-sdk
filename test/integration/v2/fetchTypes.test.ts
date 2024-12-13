3c1fc8bea44f6118208f10da691ec16cf1734d9d
ptiurin <petro.tiurin@firebolt.io>
2024-12-13 11:35:03 +0000

    test(FIR-38127): Add struct and geography tests

--- test/integration/v2/fetchTypes.test.ts
+++ test/integration/v2/fetchTypes.test.ts
@@ -59,4 +59,72 @@ describe("test type casting on fetch", () => {
       new BigNumber("-9223372036854775808")
     );
   });
+  it("select geography", async () => {
+    const firebolt = Firebolt({
+      apiEndpoint: process.env.FIREBOLT_API_ENDPOINT as string
+    });
+
+    const connection = await firebolt.connect(connectionParams);
+
+    const statement = await connection.execute(
+      "select 'POINT(1 2)'::geography"
+    );
+
+    const { data, meta } = await statement.fetchResult();
+    expect(meta[0].type).toEqual("geography");
+    const row = data[0];
+    expect((row as unknown[])[0]).toEqual(
+      "0101000020E6100000FEFFFFFFFFFFEF3F0000000000000040"
+    );
+  });
+  it("select struct", async () => {
+    const firebolt = Firebolt({
+      apiEndpoint: process.env.FIREBOLT_API_ENDPOINT as string
+    });
+
+    const connection = await firebolt.connect({
+      ...connectionParams,
+      engineName: process.env.FIREBOLT_ENGINE_NAME as string
+    });
+    await connection.execute("SET advanced_mode=1");
+    await connection.execute("SET enable_struct=1");
+    await connection.execute("SET enable_create_table_v2=true");
+    await connection.execute("SET enable_row_selection=true");
+    await connection.execute("SET prevent_create_on_information_schema=true");
+    await connection.execute("SET enable_create_table_with_struct_type=true");
+    await connection.execute("DROP TABLE IF EXISTS test_struct");
+    await connection.execute("DROP TABLE IF EXISTS test_struct_helper");
+    try {
+      await connection.execute(
+        "CREATE TABLE IF NOT EXISTS test_struct(id int not null, s struct(a array(int) not null, b datetime null) not null)"
+      );
+      await connection.execute(
+        "CREATE TABLE IF NOT EXISTS test_struct_helper(a array(int) not null, b datetime null)"
+      );
+      await connection.execute(
+        "INSERT INTO test_struct_helper(a, b) VALUES ([1, 2], '2019-07-31 01:01:01')"
+      );
+      await connection.execute(
+        "INSERT INTO test_struct(id, s) SELECT 1, test_struct_helper FROM test_struct_helper"
+      );
+
+      const statement = await connection.execute(
+        "SELECT test_struct FROM test_struct"
+      );
+
+      const { data, meta } = await statement.fetchResult();
+      expect(meta[0].type).toEqual(
+        "struct(id int, s struct(a array(int null), b timestamp null))"
+      );
+      const row = data[0];
+      expect((row as unknown[])[0]).toEqual({
+        id: 1,
+        s: { a: [1, 2], b: "2019-07-31 01:01:01" }
+      });
+    } finally {
+      // Make sure to always clean up
+      await connection.execute("DROP TABLE IF EXISTS test_struct");
+      await connection.execute("DROP TABLE IF EXISTS test_struct_helper");
+    }
+  });
 });
