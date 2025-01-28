6be39bd49e538e262d7212dd7e0d42aa161ec0ef
Stepan Burlakov <stepan.burlakov@firebolt.io>
2025-01-28 13:36:03 +0200

    added geography type test

---
+++ test/integration/v2/geography.test.ts
@@ -0,0 +1,35 @@
+import { Firebolt } from "../../../src/index";
+
+const connectionParams = {
+  auth: {
+    client_id: process.env.FIREBOLT_CLIENT_ID as string,
+    client_secret: process.env.FIREBOLT_CLIENT_SECRET as string
+  },
+  account: process.env.FIREBOLT_ACCOUNT as string,
+  database: process.env.FIREBOLT_DATABASE as string,
+  engineName: process.env.FIREBOLT_ENGINE_NAME as string
+};
+
+jest.setTimeout(100000);
+
+describe("geography", () => {
+  it("handles select geo", async () => {
+    const firebolt = Firebolt({
+      apiEndpoint: process.env.FIREBOLT_API_ENDPOINT as string
+    });
+
+    const connection = await firebolt.connect(connectionParams);
+
+    await connection.execute("SET advanced_mode=1");
+    await connection.execute("SET enable_geography=true");
+
+    const statement = await connection.execute(
+      "select 'POINT(1 1)'::geography;"
+    );
+
+    const { data, meta } = await statement.fetchResult();
+    expect(meta[0].type).toEqual("geography");
+    const row = data[0];
+    expect((row as unknown[])[0]).toEqual("0101000020E6100000FEFFFFFFFFFFEF3F000000000000F03F");
+  });
+});
